0d809bff1b0a0ed2da8c20124f247d98c00130d4
y-keysynth <ykhojojp@gmail.com>
2021-03-02 21:19:05 +0900

    HelloControllerの修正

--- SpringSample/src/main/java/com/example/demo/trySparing/HelloCotroller.java
+++ SpringSample/src/main/java/com/example/demo/trySparing/HelloCotroller.java
@@ -1,5 +1,13 @@
 package com.example.demo.trySparing;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloCotroller {
 
+	@GetMapping("/hello")
+	public String getHello() {
+		return "hello";
+	}
 }
